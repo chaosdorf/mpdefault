913a4fe2dec181ea341906700eafbeb7b34a4129
bison-- <mike.bison42@googlemail.com>
2015-02-16 00:40:33 +0100

    added reconnect, changed while to for, decreased increasing time

--- mpdefault.py
+++ mpdefault.py
@@ -4,22 +4,32 @@ uri = "http://voxsc1.somafm.com:8200"
 
 import time
 from mpd import MPDClient
-client = MPDClient()
-client.connect("localhost", 6600)
+
+
+def serve():
+    client = MPDClient()
+    client.connect("localhost", 6600)
+
+    while True:
+        time.sleep(5)
+        if len(client.playlist()) == 0:
+            client.addid(uri)
+            client.consume(1)
+            client.crossfade(0)
+            client.random(0)
+            client.repeat(0)
+            client.single(0)
+            client.play()
+            time.sleep(1)
+            
+            for i in range(10, 43):
+                client.setvol(i)
+                time.sleep(0.5)
 
 while True:
-    time.sleep(5)
-    if len(client.playlist()) == 0:
-        client.addid(uri)
-        client.consume(1)
-        client.crossfade(0)
-        client.random(0)
-        client.repeat(0)
-        client.single(0)
-        client.play()
-        time.sleep(1)
-        i = 10
-        while i < 43:
-            client.setvol(i)
-            time.sleep(1)
-            i = i+1
+    try:
+        serve()
+    except Exception as ex:
+        print ex
+        time.sleep(6.66)
+
